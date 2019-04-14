e86bf3679c188924c173cd751ee2f0280a72bf98
Paulo Renato <paulo__159@hotmail.com>
2019-04-13 23:56:47 -0300

    oi

---
+++ BarbeariaSLF/src/Frontend/TelaLogin.java
@@ -0,0 +1,74 @@
+package Frontend;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+
+public class TelaLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin frame = new TelaLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaLogin() {
+		setTitle("TELA LOGIN");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("LOGIN:");
+		lblLogin.setBounds(123, 139, 46, 14);
+		contentPane.add(lblLogin);
+		
+		JLabel lblNewLabel = new JLabel("SENHA:");
+		lblNewLabel.setBounds(123, 164, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(169, 136, 104, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(169, 161, 104, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnCadastrar = new JButton("CADASTRAR");
+		btnCadastrar.setBounds(155, 192, 118, 23);
+		contentPane.add(btnCadastrar);
+		
+		JButton btnEsqueciASenha = new JButton("ESQUECI A SENHA");
+		btnEsqueciASenha.setBounds(151, 227, 155, 23);
+		contentPane.add(btnEsqueciASenha);
+	}
+}
