c1adda8f6bbdc425545464a71256fe9208dc6918
Eduardo Hollanda <eduardohollanda14@gmail.com>
2019-04-16 23:36:47 -0300

    comecando novamente

---
+++ src/view/RecupSenha.java
@@ -0,0 +1,97 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class RecupSenha extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField loginUsuario;
+	private JPasswordField novaSenha;
+	private JLabel lblDigiteASenha;
+	private JTextField confirmacaoSenha;
+	private JButton btnCancelar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RecupSenha frame = new RecupSenha();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RecupSenha() {
+		setTitle("RECUPERAR SENHA");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 292, 399);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel jlbUsuario = new JLabel("USUARIO :");
+		jlbUsuario.setBounds(42, 35, 73, 14);
+		contentPane.add(jlbUsuario);
+		
+		loginUsuario = new JTextField();
+		loginUsuario.setBounds(36, 60, 184, 20);
+		contentPane.add(loginUsuario);
+		loginUsuario.setColumns(10);
+		
+		JLabel lblNovaSenha = new JLabel("NOVA SENHA:");
+		lblNovaSenha.setBounds(41, 109, 89, 14);
+		contentPane.add(lblNovaSenha);
+		
+		novaSenha = new JPasswordField();
+		novaSenha.setBounds(36, 134, 184, 20);
+		contentPane.add(novaSenha);
+		
+		lblDigiteASenha = new JLabel("DIGITE A SENHA NOVAMENTE :");
+		lblDigiteASenha.setBounds(42, 181, 178, 14);
+		contentPane.add(lblDigiteASenha);
+		
+		confirmacaoSenha = new JTextField();
+		confirmacaoSenha.setBounds(42, 206, 178, 20);
+		contentPane.add(confirmacaoSenha);
+		confirmacaoSenha.setColumns(10);
+		
+		JButton btnTrocarSenha = new JButton("TROCAR SENHA");
+		btnTrocarSenha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnTrocarSenha.setBounds(10, 272, 119, 23);
+		contentPane.add(btnTrocarSenha);
+		
+		btnCancelar = new JButton("CANCELAR");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCancelar.setBounds(154, 272, 89, 23);
+		contentPane.add(btnCancelar);
+		
+
+	}
+}
---
+++ src/view/TelaLogin.java
@@ -0,0 +1,82 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TelaLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField loginUsuario;
+	private JPasswordField senhaUsuario;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin frame = new TelaLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaLogin() {
+		setTitle("LOGIN");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 355, 198);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel jlbusuario = new JLabel("LOGIN:");
+		jlbusuario.setBounds(58, 26, 46, 14);
+		contentPane.add(jlbusuario);
+		
+		loginUsuario = new JTextField();
+		loginUsuario.setBounds(114, 23, 104, 20);
+		contentPane.add(loginUsuario);
+		loginUsuario.setColumns(10);
+		
+		JLabel jlbsenha = new JLabel("SENHA:");
+		jlbsenha.setBounds(58, 66, 46, 14);
+		contentPane.add(jlbsenha);
+		
+		JButton btnEntrar = new JButton("ENTRAR");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		senhaUsuario = new JPasswordField();
+		senhaUsuario.setBounds(114, 63, 104, 20);
+		contentPane.add(senhaUsuario);
+		btnEntrar.setBounds(29, 112, 118, 23);
+		contentPane.add(btnEntrar);
+		
+		JButton btnEsqueciASenha = new JButton("ESQUECI A SENHA");
+		btnEsqueciASenha.setBounds(173, 112, 155, 23);
+		contentPane.add(btnEsqueciASenha);
+	
+		
+	}
+}
